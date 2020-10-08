675120d0810cd76777a1e6f166a7e166b0b8b98e
Elijah Dangerfield <45648517+Elijah-Dangerfield@users.noreply.github.com>
2020-10-08 08:14:16 -0500

    ranmes users to userData

--- app/src/main/java/com/dangerfield/hiltplayground/ui/home/HomeData.kt
+++ app/src/main/java/com/dangerfield/hiltplayground/ui/home/HomeData.kt
@@ -5,7 +5,7 @@ import com.dangerfield.hiltplayground.models.CarouselData
 import com.dangerfield.hiltplayground.models.HeaderData
 import com.dangerfield.hiltplayground.models.UserData
 
-data class HomeData(val blogData: List<BlogData> = listOf(), val users: List<UserData> = listOf()) {
+data class HomeData(val blogData: List<BlogData> = listOf(), val userData: List<UserData> = listOf()) {
 
     //TODO maybe have an interface for all screen data to conform to?
     fun toDataList(): List<Any> {
@@ -14,7 +14,7 @@ data class HomeData(val blogData: List<BlogData> = listOf(), val users: List<Use
 
     private fun createUsers(): List<Any> {
         return listOf(HeaderData("Users")) + CarouselData(
-            this.users
+            this.userData
         )
     }
 
